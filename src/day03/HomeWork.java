bc98de95da6c8d8fb1949c09c8cb5e040b15fd31
1357613573 <1357613573@qq.com>
2020-06-17 08:57:13 +0800

    丁海杨作业第一次项目结构上传

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -3,8 +3,9 @@ package day03;
 public class HomeWork {
     //打印九九乘法表
     public static void main(String[] args) {
-System.out.println("打印成功")；
-    
+        System.out.println("打印成功");
+        System.out.println("等腰三角形");
+
     }
 
 
