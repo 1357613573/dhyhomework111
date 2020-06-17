61230b7ecd7349e7cc43490d113e509131b6dd26
1357613573 <1357613573@qq.com>
2020-06-17 18:36:09 +0800

    丁海杨作业第一次项目结构上传

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -1,17 +1,38 @@
 package day03;
 
 public class HomeWork {
-    //打印九九乘法表
-    public static void main(String[] args) {
-        System.out.println("打印成功");
-        System.out.println("等腰三角形");
+
+    public static void circle(int row) {
+        for (int i = 1; i <=row; i++) {
+            for (int j=1;j<=row-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void sum(){
+        int sum=2;
+
+        for(int i=2;i<=100;i++){
+            for(int j=2;j<i;j++){
+                if(i%j == 0 && i!=2){
+                    break;
+                }else if(j+1==i){
+                    sum+=i;
+                }
+            }
+        }
+        System.out.println("100以内全部素数和为"+sum);
+
 
     }
+    public static void main(String[] args) {
 
+        circle(5);//d打印等腰三角形
+        sum();//打印100以内素数和
 
-
-
-
-
-    //打印等腰三角形
+    }
 }
